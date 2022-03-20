1047a70b809a71813bd251efc8e849b6db59da93
toddmcintire <mail@toddmcintire.com>
2022-03-20 16:38:10 -0500

    added error if choco is not installed

--- gui.py
+++ gui.py
@@ -1,26 +1,22 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 from installer import *
-
-#updateText = "Updated!!!"
-
-def updater():
-    #global updateText
-    display.config(text="updateText")
-
-def updaterTwo():
-    display.config(text=getExecution())
 
 root = Tk()
 frame = ttk.Frame(root, padding=10)
 display = Label(frame, text="croc gui")
+notebook = ttk.Notebook(frame)
+
+if is_tool("choco") == False:
+    messagebox.showerror("showerror", "choco is not installed please install first")
 
 frame.grid()
 root.title("croc gui")
 display.grid(column=3, row=1)
-ttk.Button(frame, text="update text", command=lambda : updater()).grid(column=1, row=2)
-ttk.Button(frame, text="update two", command=lambda : updaterTwo()).grid(column=2, row=2)
-ttk.Button(frame, text="check execution policy", command=lambda : getExecution() ).grid(column=4,row=3)
-ttk.Button(frame, text="change execution policy", command=lambda : display.config(text=changerExecution())).grid(column=4,row=4)
+ttk.Button(frame, text="check if choco", command=lambda : display.config(text=is_tool("choco"))).grid(column=4,row=4)
+ttk.Button(frame, text="install choco", command=lambda : display.config(text=installChoco())).grid(column=4,row=5)
 ttk.Button(frame, text="Quit", command=root.destroy).grid(column=5, row=5)
 root.mainloop()
+
+#TODO: check if user has choco installed,  if they dont run an alert window
