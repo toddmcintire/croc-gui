8cedc7aba87ec6e0ae6943ecef13099763725bbb
toddmcintire <mail@toddmcintire.com>
2022-03-20 20:40:17 -0500

    removed unused line and added TODO

--- gui.py
+++ gui.py
@@ -6,7 +6,6 @@ from installer import *
 root = Tk()
 frame = ttk.Frame(root, padding=10)
 display = Label(frame, text="croc gui")
-notebook = ttk.Notebook(frame)
 
 # Quits the program if choco is not installed and quits
 if is_tool("choco") == False:
@@ -17,6 +16,7 @@ frame.grid()
 root.title("croc gui")
 display.grid(column=3, row=1)
 ttk.Button(frame, text="check if choco", command=lambda : display.config(text=is_tool("choco"))).grid(column=4,row=4)
+#TODO: change below button command to install croc function
 ttk.Button(frame, text="install choco", command=lambda : display.config(text=installChoco())).grid(column=4,row=5)
 ttk.Button(frame, text="Quit", command=root.destroy).grid(column=5, row=5)
 root.mainloop()
