d09dd317dc43645aa36f1002525cca7faf6c1d63
Todd McIntire <mail@toddmcintire.com>
2022-05-01 11:10:31 -0500

    added endcase for windows and mac to run curl script if all else fails

--- crocinstaller.js
+++ crocinstaller.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env zx
 //script to install croc based on os type
-void async function test () {
+void async function test() {
+
+	function endCase() {
+		const install = question(`Would you like to try to install via the instalall script? Y/N `);
+		const yes = [`YES`, `Yes`, `yes`, `Y`, `y`];
+		const no = [`NO`, `No`, `no`, `N`, `n`];
+		install.includes(yes) === true
+			? $`curl https://getcroc.schollz.com | bash`
+			: install.includes(no) === true
+				? continue
+				: //error not a proper responce;
+	}
+
 	if (os.type() === `Darwin`) {
 		const brew = which.sync('brew', {nothrow: true});
 		const port = which.sync(`port`, {nothrow: true});
@@ -8,19 +20,18 @@ void async function test () {
 			? $`brew install croc`
 			: port != null
 				? $`sudo port selfupdate && sudo port install croc`
-				: console.log(`please install either brew or port`); //add end case
+				: console.log(`please install either brew or port`), endCase();
 	}
 	if (os.type() === `Linux`) {
 		//TODO: somehow check what type 
 	}
 	if (os.type() === `Windows_NT`) {
-		var scoop;
-		var choco = which.sync(`choco`, {nothrow: true});
+		let scoop;
+		const choco = which.sync(`choco`, {nothrow: true});
 		choco != null
 			? $`choco install croc`
 			: scoop != null
 				? $`scoop install croc`
-				: console.log(`please install either choco or scoop`); //add end case
+				: console.log(`please install either choco or scoop`), endCase();
 	}
-	//TODO: create function above that if all three faul 
 }()
