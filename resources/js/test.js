974bfd6c0549a3a7f47d4e7d93210d01338cae8e
Todd McIntire <mint@Todds-MacBook-Pro.local>
2022-05-16 09:31:45 -0500

    changed textConfirmation funtcion to async

--- resources/js/test.js
+++ resources/js/test.js
@@ -2,9 +2,9 @@ function setText() {
     document.getElementById('question').innerHTML = `do you want to run test command?`;
 }
 
-function textConfirmation() {
-    //let commandResponse = await Neutralino.os.execCommand('echo "Hello World!"');
-    document.getElementById('response').innerHTML = `${commandResponse.stdOut}`;
+async function textConfirmation() {
+    let  commandResponse = await Neutralino.os.execCommand('echo "hello world"');
+    document.getElementById('response').innerHTML = commandResponse.stdOut;
 }
 
 Neutralino.init();
